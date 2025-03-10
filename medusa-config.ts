0786e12828c17c60a08baadcd634ae92ec096764
Your Name <mhal236@uky.edu>
2025-03-10 17:59:32 -0400

    email

--- medusa-config.ts
+++ medusa-config.ts
@@ -16,4 +16,18 @@ module.exports = defineConfig({
   admin: {
     backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
+  plugins: [
+    {
+      resolve: `medusa-plugin-sendgrid`,
+      options: {
+        api_key: process.env.SENDGRID_API_KEY,
+        from: process.env.SENDGRID_FROM,
+        order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID,
+        order_shipped_template: process.env.SENDGRID_ORDER_SHIPPED_ID,
+        order_canceled_template: process.env.SENDGRID_ORDER_CANCELED_ID,
+        user_password_reset_template: process.env.SENDGRID_USER_PASSWORD_RESET_ID,
+        customer_password_reset_template: process.env.SENDGRID_CUSTOMER_PASSWORD_RESET_ID,
+      },
+    },
+  ],
 })
